d1209e36670fc46dd97fc53920bffd02944a6e3a
victoramccomp <victor.am.ccomp@gmail.com>
2016-06-13 20:35:00 -0300

    Passo 4: Teste 1: Suportar delimitadores customizados.

--- StringCalculator/StringCalculator/Calculadora.cs
+++ StringCalculator/StringCalculator/Calculadora.cs
@@ -2,24 +2,47 @@
 
 namespace StringCalculator
 {
-    internal class Calculadora
+    public class Calculadora
     {
-        public Calculadora()
-        {
-        }
+        private string delimitadorCustomizado = "";
 
-        public int Adicionar(string valor)
+        public int Somar(string numeros)
         {
-            if (string.IsNullOrWhiteSpace(valor))
+            if (string.IsNullOrWhiteSpace(numeros))
                 return 0;
 
-            var valores = valor.Split(',', '\n');
+            if (PossuiDelimitadorCustomizado(numeros))
+                numeros = ObterNumerosComDelimitadorCustomizado(numeros);
 
+            return SomarNumeros(numeros);
+        }
+
+        private static bool PossuiDelimitadorCustomizado(string numeros)
+        {
+            return numeros.StartsWith("//");
+        }
+
+        private string ObterNumerosComDelimitadorCustomizado(string numeros)
+        {
+            var posicaoPrimeiraNovaLinha = numeros.IndexOf('\n');
+            delimitadorCustomizado = numeros.Substring(2, posicaoPrimeiraNovaLinha - 2);
+            numeros = numeros.Substring(posicaoPrimeiraNovaLinha + 1);
+            return numeros;
+        }
+
+        private int SomarNumeros(string numeros)
+        {
+            var numeroses = numeros.Split(ObterDelimitadores(), StringSplitOptions.RemoveEmptyEntries);
             int total = 0;
-            foreach(var numero in valores)
+            foreach (var numero in numeroses)
                 total += int.Parse(numero);
-
             return total;
         }
+
+        private string[] ObterDelimitadores()
+        {
+            return new string[] { ",", "\n", delimitadorCustomizado };
+        }
+
     }
 }
--- StringCalculator/StringCalculator/CalculadoraTest.cs
+++ StringCalculator/StringCalculator/CalculadoraTest.cs
@@ -15,28 +15,35 @@ namespace StringCalculator
         }
 
         [TestMethod]
-        public void SeAdicionarValorVazio_RetornaZero()
+        public void SeSomarValorVazio_RetornaZero()
         {
-            Assert.AreEqual(0, calculadora.Adicionar(""));
+            Assert.AreEqual(0, calculadora.Somar(""));
         }
 
         [TestMethod]
-        public void SeAdicionarUmNumero_RetornaOMesmoNumero()
+        public void SeSomarUmNumero_RetornaOMesmoNumero()
         {
-            Assert.AreEqual(1, calculadora.Adicionar("1"));
+            Assert.AreEqual(1, calculadora.Somar("1"));
         }
 
         [TestMethod]
-        public void SeAdicionarDoisOuMaisNumeros_RetornaASoma()
+        public void SeSomarDoisOuMaisNumeros_RetornaASoma()
         {
-            Assert.AreEqual(5, calculadora.Adicionar("2,3"));
-            Assert.AreEqual(19, calculadora.Adicionar("5,3,7,4"));
+            Assert.AreEqual(5, calculadora.Somar("2,3"));
+            Assert.AreEqual(19, calculadora.Somar("5,3,7,4"));
         }
 
         [TestMethod]
         public void SeSeparadorForNovaLinha_RetornaASoma()
         {
-            Assert.AreEqual(19, calculadora.Adicionar("5\n3,7,4"));
+            Assert.AreEqual(19, calculadora.Somar("5\n3,7,4"));
+        }
+
+        [TestMethod]
+        public void SeHouverSeparadorCustomizado_RetornaASoma()
+        {
+            Assert.AreEqual(6, calculadora.Somar("//;\n1;2;3"));
+            Assert.AreEqual(6, calculadora.Somar("//victor\n1victor2victor3"));
         }
     }
 }
