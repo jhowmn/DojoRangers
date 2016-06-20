837e42a1b441bd943e8a6d4c7b0294c6a3842cae
victoramccomp <victor.am.ccomp@gmail.com>
2016-06-20 19:57:19 -0300

    Passo 2: Canaleta e um acerto.

--- BowlingGame/BowlingGame/BowlingGameTest.cs
+++ BowlingGame/BowlingGame/BowlingGameTest.cs
@@ -17,7 +17,27 @@ namespace BowlingGame
         [TestMethod]
         public void CanRoll()
         {
-            game.Roll();
+            game.Roll(0);
+        }
+
+        [TestMethod]
+        public void ThrowBall()
+        {
+            for(var i = 0; i < 20; i++)
+                game.Roll(0);
+
+            Assert.AreEqual(0, game.Score());
+        }
+
+        [TestMethod]
+        public void IdiotsGame()
+        {
+            game.Roll(2);
+
+            for (var i = 0; i < 19; i++)
+                game.Roll(0);
+
+            Assert.AreEqual(2, game.Score());
         }
     }
 }
--- BowlingGame/BowlingGame/Game.cs
+++ BowlingGame/BowlingGame/Game.cs
@@ -4,13 +4,20 @@ namespace BowlingGame
 {
     public class Game
     {
+        private int score = 0;
+
         public Game()
         {
         }
 
-        public void Roll()
+        public void Roll(int pontos)
         {
-            
+            score += pontos;
+        }
+
+        public int Score()
+        {
+            return score;
         }
     }
 }
