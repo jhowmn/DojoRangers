bd74c55f6b1a13d179a5bb490d0c2d2c49481685
victoramccomp <victor.am.ccomp@gmail.com>
2016-06-20 21:02:35 -0300

    All done! =) Hora do lanche

--- BowlingGame/BowlingGame/BowlingGameTest.cs
+++ BowlingGame/BowlingGame/BowlingGameTest.cs
@@ -6,25 +6,18 @@ namespace BowlingGame
     [TestClass]
     public class BowlingGameTest
     {
-        private static Game game;
+        private Game game;
 
-        [ClassInitialize]
-        public static void InitializeClass(TestContext context)
+        [TestInitialize]
+        public void InitializeClass()
         {
             game = new Game();
         }
 
         [TestMethod]
-        public void CanRoll()
+        public void GutterGame()
         {
-            game.Roll(0);
-        }
-
-        [TestMethod]
-        public void ThrowBall()
-        {
-            for(var i = 0; i < 20; i++)
-                game.Roll(0);
+            RollMany(20, 0);
 
             Assert.AreEqual(0, game.Score());
         }
@@ -32,12 +25,45 @@ namespace BowlingGame
         [TestMethod]
         public void IdiotsGame()
         {
+            RollMany(20, 1);
+
+            Assert.AreEqual(20, game.Score());
+        }
+
+        [TestMethod]
+        public void CalcSpare()
+        {
+            game.Roll(3);
+            game.Roll(7);
+            game.Roll(4);
             game.Roll(2);
 
-            for (var i = 0; i < 19; i++)
-                game.Roll(0);
+            RollMany(16, 0);
 
-            Assert.AreEqual(2, game.Score());
+            Assert.AreEqual(20, game.Score());
+        }
+
+        [TestMethod]
+        public void CalcStrike()
+        {
+            game.Roll(10);
+            game.Roll(3);
+            game.Roll(4);
+
+            Assert.AreEqual(24, game.Score());
+        }
+
+        [TestMethod]
+        public void PerfectGame()
+        {
+            RollMany(20, 10);
+            Assert.AreEqual(300, game.Score());
+        }
+
+        private void RollMany(int times, int pins)
+        {
+            for (var i = 0; i < times; i++)
+                game.Roll(pins);
         }
     }
 }
--- BowlingGame/BowlingGame/Game.cs
+++ BowlingGame/BowlingGame/Game.cs
@@ -4,20 +4,51 @@ namespace BowlingGame
 {
     public class Game
     {
-        private int score = 0;
+        private int[] throws = new int[21];
+        private int currentIndex = 0;
 
-        public Game()
+        public void Roll(int pins)
         {
-        }
-
-        public void Roll(int pontos)
-        {
-            score += pontos;
+            throws[currentIndex] = pins;
+            currentIndex++;
         }
 
         public int Score()
         {
+            int score = 0;
+            int firstInFrame = 0;
+
+            for(int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(firstInFrame))
+                {
+                    score += 10 + throws[firstInFrame + 1] + throws[firstInFrame + 2];
+                    firstInFrame++;
+
+                }
+                else if (IsSpare(firstInFrame))
+                {
+                    score += 10 + throws[firstInFrame + 2];
+                    firstInFrame += 2;
+                }
+                else
+                {
+                    score += throws[firstInFrame] + throws[firstInFrame + 1];
+                    firstInFrame += 2;
+                }
+            }
+
             return score;
+        }
+
+        private bool IsStrike(int firstInFrame)
+        {
+            return throws[firstInFrame] == 10;
+        }
+
+        private bool IsSpare(int i)
+        {
+            return throws[i] + throws[i + 1] == 10;
         }
     }
 }
