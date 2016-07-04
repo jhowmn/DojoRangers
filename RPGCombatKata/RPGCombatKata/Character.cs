638e8b6649a291b96dd4181dce869524b72d4dcb
victoramccomp <victor.am.ccomp@gmail.com>
2016-07-04 19:30:29 -0300

    Iteration 1

---
+++ RPGCombatKata/RPGCombatKata/Character.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace RPGCombatKata
+{
+    public class Character
+    {
+        public int Level { get; private set; }
+        public int Health { get; private set; }
+        public bool IsAlive
+        {
+            get
+            {
+                return Health > 0;
+            }
+        }
+
+        public Character()
+        {
+            Level = 1;
+            Health = 1000;
+        }
+
+        public void Attack(Character foe, int damage)
+        {
+            foe.Health -= damage;
+            if (!foe.IsAlive)
+                foe.Health = 0;
+        }
+
+        public void Heal(Character foe2, int healthHealed)
+        {
+            if (foe2.Health + healthHealed > 1000)
+                foe2.Health = 1000;
+            else
+                foe2.Health += healthHealed;
+        }
+    }
+}
---
+++ RPGCombatKata/RPGCombatKata/CharacterTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RPGCombatKata
+{
+    [TestClass]
+    public class CharacterTest
+    {
+        private Character CreateOneFoe()
+        {
+            return new Character();
+        }
+
+        [TestMethod]
+        public void CreateDefaultCharacter()
+        {
+            Character c = CreateOneFoe();
+            Assert.AreEqual(1, c.Level);
+            Assert.AreEqual(1000, c.Health);
+            Assert.AreEqual(true, c.IsAlive);
+        }
+
+        [TestMethod]
+        public void IfDamageIsHigherThanHealth_CharacterIsDead()
+        {
+            Character foe1 = CreateOneFoe();
+            Character foe2 = CreateOneFoe();
+
+            foe1.Attack(foe2, 2000);
+
+            Assert.AreEqual(0, foe2.Health);
+            Assert.IsFalse(foe2.IsAlive);
+        }
+
+        [TestMethod]
+        public void IfCharacterIsHealedWithFullHealth_HealingHasNoEffect()
+        {
+            Character foe1 = CreateOneFoe();
+            Character foe2 = CreateOneFoe();
+
+            foe1.Heal(foe2, 100);
+
+            Assert.AreEqual(1000, foe2.Health);
+        }
+    }
+}
